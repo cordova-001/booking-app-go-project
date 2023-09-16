cf712fd00f69f3d032e598a90d2e2d3fbd748dfc
Ahmad <aabiola610@gmail.com>
2023-09-16 10:57:30 +0100

    var and const

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+
+	var conferenceName = "EYECON 2023"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welome to", conferenceName, "Exeptional ")
+	fmt.Println("We have a total of", conferenceTickets, "Ticket and the remaining ticket is", remainingTickets)
+	fmt.Println("This is where you get your ticket")
+
 }
